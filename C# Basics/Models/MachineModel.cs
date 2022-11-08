a7e77dbf68fc38f746e6ee52736876def52eca1e
Ediz Redzhepov <edizonfire@gmail.com>
2022-11-08 17:37:44 +0200

    C# Basics assignment

---
+++ C# Basics/Models/MachineModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basics.Models
+{
+    public class MachineModel
+    {
+        private string _machineId;
+        public int Quantity { get; set; }
+        bool isOn = false;
+        public string MachineId
+        {
+            get { return _machineId; }
+            set { _machineId = value; }
+        }
+
+        public void TurnOn()
+        {
+            if (isOn)
+            {
+                Console.WriteLine("The machine is already on.");
+            }
+            else
+            {
+                Console.WriteLine("The machine is turned on.");
+                isOn = true;
+            }
+        }
+
+        public void TurnOff()
+        {
+            if (isOn)
+            {
+                Console.WriteLine("The machine is turned off.");
+                isOn = false;
+            }
+            else
+            {
+                Console.WriteLine("The machine is already turned off.");
+            }
+        }
+    }
+}
---
+++ C# Basics/Program.cs
@@ -0,0 +1,9 @@
+﻿using C_Sharp_Basics.Models;
+
+ComputerModel computer = new ComputerModel();
+
+MachineCreatorApp.CreateComputer(computer);
+Console.Clear();
+
+MachineCreatorApp.GetComputerInfo(computer);
+MachineCreatorApp.TurnOnOrOff(computer);
