e1a68298155d32f68a029c6c94ea1ae58038f408
Ediz Redzhepov <edizonfire@gmail.com>
2022-11-10 19:49:23 +0200

    Arrays, collections, and generics assignment

---
+++ Arrays_Collections_Generics/Collection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Collections_Generics
+{
+    public class Collection
+    {
+        public static string AnswerFromConsole(string request)
+        {
+            Console.Write(request);
+            return Console.ReadLine();
+        }
+
+        public static void GetItem<Thing>(Thing[] array)
+        {
+            int index = int.Parse(AnswerFromConsole("Enter the index for the item you want to get: "));
+            Console.WriteLine($"{array[index]} is at index {index}.");
+        }
+
+        public static void SetItem<Thing>(Thing[] array, Thing item)
+        {
+            int index = int.Parse(AnswerFromConsole("Enter the index for the item you want to set: "));
+            array[index] = item;
+            Console.WriteLine("Modified array:");
+            foreach (var thing in array)
+            {
+                Console.Write(thing + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void SwapItems<Thing>(Thing[] array)
+        {
+            int index = int.Parse(AnswerFromConsole("Enter the index for the first item you want to swap: "));
+            int index2 = int.Parse(AnswerFromConsole("Enter the index for the second item you want to swap: "));
+            Thing temp = array[index];
+            array[index] = array[index2];
+            array[index2] = temp;
+            Console.WriteLine("Modified array:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Arrays_Collections_Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using Arrays_Collections_Generics;
+
+string[] strings = { "one", "two", "three", "four", "five" };
+int[] ints = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine($"Initial arrays:");
+foreach (var thing in strings)
+{
+    Console.Write(thing + " ");
+}
+
+Console.WriteLine();
+foreach (var thing in ints)
+{
+    Console.Write(thing + " ");
+}
+
+Console.WriteLine();
+
+var answer = Collection.AnswerFromConsole("Do you want to work with an array of string or integers? 's' or 'i'.");
+if (answer == "s")
+{
+    Collection.GetItem(strings);
+    answer = Collection.AnswerFromConsole("Enter a string you wish to set: ");
+    Collection.SetItem(strings, answer);
+    Collection.SwapItems(strings);
+}
+else if (answer == "i")
+{
+    Collection.GetItem(ints);
+    answer = Collection.AnswerFromConsole("Enter an integer you wish to set: ");
+    Collection.SetItem(ints, int.Parse(answer));
+    Collection.SwapItems(ints);
+}
+
+Console.ReadLine();
