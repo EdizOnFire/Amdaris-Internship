1b659f0b55f4fb1ce8b89b9f95fc1bb0baab8739
Ediz Redzhepov <edizonfire@gmail.com>
2022-11-17 15:42:39 +0200

    File System and Streams assignment

--- File System and Streams/Program.cs
+++ File System and Streams/Program.cs
@@ -4,7 +4,7 @@
 
 byte[] key = { 145, 12, 32, 245, 98, 132, 98, 214, 6, 77, 131, 44, 221, 3, 9, 50 };
 byte[] iv = { 15, 122, 132, 5, 93, 198, 44, 31, 9, 39, 241, 49, 250, 188, 80, 7 };
-byte[] data = { 1, 2, 3, 4, 5, 7 };
+byte[] data = { 1, 2, 3, 4, 5 };
 using (SymmetricAlgorithm algorithm = Aes.Create())
 using (ICryptoTransform encryptor = algorithm.CreateEncryptor(key, iv))
 using (Stream f = File.Create("encrypted.bin"))
