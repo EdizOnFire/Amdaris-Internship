9361eac9e7868159612d2410e31b13ae30ff17a2
Ediz Redzhepov <edizonfire@gmail.com>
2022-11-18 15:46:46 +0200

    Singleton assignment complete

---
+++ Creational Patterns/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton instance1 = Singleton.Instance;
+            Singleton instance2 = Singleton.Instance;
+
+            //When app runs, the second instance is not being created
+
+            instance1.WriteMessage("Hello from instance1.");
+            instance2.WriteMessage("Hello from instance2.");
+        }
+    }
+}
---
+++ Creational Patterns/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static int count = 0;
+        private static readonly object InstanceLock = new object();
+        private Singleton()
+        {
+            count++;
+            WriteLine($"{count} number of instances created.");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (InstanceLock)
+                    {
+                        instance = new Singleton();
+                    }
+
+                }
+
+                return instance;
+            }
+        }
+
+        public void WriteMessage(string message)
+        {
+            WriteLine(message);
+            ReadLine();
+        }
+    }
+}
