15ec73352072874e98968d8f5aab3efa3e7160c7
Ediz Redzhepov <edizonfire@gmail.com>
2022-12-06 12:32:33 +0200

    EF Core Fundamentals assignment

---
+++ EF Core Fundamentals/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using EFCoreFundamentals.Models;
+
+namespace EFCoreFundamentals.Data
+{
+    public class AppDbContext : DbContext
+    {
+        readonly string constring = "Data Source=EDIZONFIRE\\SQLEXPRESS;Initial Catalog=Blogs;Integrated Security=True;Encrypt=False";
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            options.UseSqlServer(constring);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(b => b.Book)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(bi => bi.BookId);
+
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(a => a.Author)
+                .WithMany(ba => ba.Book_Authors)
+                .HasForeignKey(ai => ai.AuthorId);
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Book_Author> Book_Authors { get; set; }
+    }
+}
