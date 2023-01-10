9380a96adde05625958bb006330ad0d32fb5a4cb
Ediz Redzhepov <edizonfire@gmail.com>
2023-01-10 12:42:02 +0200

    Catalog name and connection string update

--- AudioEditor/AudioEditor.Infrastructure/AppDbContext.cs
+++ AudioEditor/AudioEditor.Infrastructure/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace AudioEditor.Infrastructure
 {
     public class AppDbContext : DbContext
     {
-        readonly string constring = "Data Source=EDIZONFIRE\\SQLEXPRESS;Initial Catalog=Blogs;Integrated Security=True;Encrypt=False";
+        readonly string constring = "Data Source=EDIZONFIRE\\SQLEXPRESS;Initial Catalog=Database;Integrated Security=True;Encrypt=False";
         protected override void OnConfiguring(DbContextOptionsBuilder options) =>
             options.UseSqlServer(constring);
 
