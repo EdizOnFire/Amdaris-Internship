b218c28e77c5e7196c926573bb9f61d755dd8e8b
Ediz Redzhepov <edizonfire@gmail.com>
2023-01-13 14:20:12 +0200

    Added login and register page

--- AudioEditor-FrontEnd/src/pages/Home/Home.jsx
+++ AudioEditor-FrontEnd/src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             <h2>Welcome to </h2>
             <h1>Audio Editor</h1>
             <p>Click <Link to="/upload">here</Link> to upload your audio files.</p>
-            <img src='/assets/images/homescreen.png' alt='Sorry.' id='homeImage' />
+            {/* <img src='/assets/images/homescreen.png' alt='Sorry.' id='homeImage' /> */}
         </section>
     );
 };
--- AudioEditor-FrontEnd/src/pages/index.js
+++ AudioEditor-FrontEnd/src/pages/index.js
@@ -1,6 +1,8 @@
 import Home from "./Home/Home";
 import Upload from "./Upload/Upload";
+import Register from "./Register/Register";
 import NotFound from "./NotFound/NotFound";
-import YourAudioFiles from "./YourAudioFiles/YourAudioFiles";
+import Login from "./Login/Login";
+import Browse from "./Browse/Browse";
 
-export { Home, Upload, NotFound, YourAudioFiles }
+export { Home, Upload, Browse, Login, Register, NotFound }
