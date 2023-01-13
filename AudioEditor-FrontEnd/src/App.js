1c1fc30ac772e12b9341cefe5b6c263a2e95861a
Ediz Redzhepov <edizonfire@gmail.com>
2023-01-13 14:17:27 +0200

    New router added

--- AudioEditor-FrontEnd/src/App.js
+++ AudioEditor-FrontEnd/src/App.js
@@ -1,33 +1,59 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ItemProvider } from "./contexts/ItemContext";
-import { Header, Footer } from "./components";
-import { Home, Upload, NotFound, YourAudioFiles } from "./pages";
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { Theme } from './services/theme'
+import { Home, Upload, Browse, Login, Register, NotFound } from "./pages";
+import { Theme } from "./services/theme";
+import AudioFiles, { audioFilesLoader } from "./components/AudioFiles/AudioFiles";
+import Layout from "./components/Layout/Layout";
+import { submitLogin } from "./pages/Login/Login";
+import { submitRegister } from "./pages/Register/Register";
 
-const queryClient = new QueryClient()
-
-function App() {
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "browse",
+          element: <Browse />,
+          children: [
+            {
+              index: true,
+              element: <AudioFiles />,
+              loader: audioFilesLoader,
+            },
+          ],
+        },
+        {
+          path: "upload",
+          element: <Upload />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+          action: submitLogin
+        },
+        {
+          path: "register",
+          element: <Register />,
+          action: submitRegister
+        },
+        {
+          path: "*",
+          element: <NotFound />,
+        },
+      ],
+    },
+  ]);
   return (
-    <BrowserRouter>
-     <Theme>
-      <QueryClientProvider client={queryClient}>
-        <Header />
-        <ItemProvider>
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/your-audio-files" element={<YourAudioFiles />} />
-              <Route path="/upload" element={<Upload />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </ItemProvider>
-        <Footer />
-      </QueryClientProvider>
-      </Theme>
-    </BrowserRouter>
+    <Theme>
+      <ItemProvider>
+        <RouterProvider router={router} />
+      </ItemProvider>
+    </Theme>
   );
 }
-
-export default App;
